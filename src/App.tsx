d3e1102831f280b8f087994965fa4cdab6c36037
Antero Ramos <antero.ramos@five9.com>
2025-11-25 17:55:21 +0000

    feat: update to v4 deploy

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/hello-world-page/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auctions" element={<Auctions />} />
